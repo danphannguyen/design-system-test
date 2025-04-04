f69ea1f47a8a037ea2d46301294b661bde6a55b5
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-04-04 09:59:08 +0200

    fix: :bug: fix 404 npm error

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # README
 
-This is the source for the > npx danphannguyen package that you can run in your preffered node env.
+This is the source for the > npx dan-phan-nguyen package that you can run in your preffered node env.
 
-https://www.npmjs.com/danphannguyen
+https://www.npmjs.com/dan-phan-nguyen
+
+# Publier un package npm
+
+Créer votre token npm, rdv sur le site dans "Profil" et générer votre token ^^ :3
+
+Ensuite créer un .npmrc et modifiez les valeurs
+
+```npm publish --access public```
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cText = (text, color) => {
 };
 console.log(cText("###################################", YELLOW));
 console.log(cText("  Hello!", GREEN));
-console.log(cText("  I'm danphannguyen, some call me firstname!", YELLOW));
-console.log(cText(`  GitHub: https://github.com/danphannguyen`, WHITE));
+console.log(cText("  I'm dan-phan-nguyen, some call me firstname!", YELLOW));
+console.log(cText(`  GitHub: https://github.com/dan-phan-nguyen`, WHITE));
 console.log(cText("  Have a great life!", GREEN));
 console.log(cText("###################################", YELLOW));
